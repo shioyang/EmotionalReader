3971b0341fbe1be7172d1fb4a67ebc76a28b363e
Tomohiro SHIOYA <shioyang4321@gmail.com>
2016-11-19 22:37:15 +0900

    Use indico.emotion API

--- ERServer/routes/er.js
+++ ERServer/routes/er.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+var indico = require('indico.io');
+//var response = function(res) { console.log(res); }
+var logError = function(err) { console.log(err); }
+
 /* GET API list. */
 router.get('/', function(req, res, next) {
   res.render('api_list', { title: 'ER API List' });
+});
+
+/* POST */
+router.get('/emotion', function(req, res){
+//router.post('/emotion', function(req, res){
+  // single example
+  indico.emotion("I did it. I got into Grad School. Not just any program, but a GREAT program. :-)")
+    .then(function(res_emotion){
+      res.send(res_emotion);
+    })
+    .catch(logError);
 });
 
 module.exports = router;
