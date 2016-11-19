f6e2e6ee7bacf9676a788edf091b29b4862ec7f1
Tomohiro SHIOYA <shioyang4321@gmail.com>
2016-11-19 21:45:01 +0900

    Create new path

--- ERServer/routes/er.js
+++ ERServer/routes/er.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET API list. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('api_list', { title: 'ER API List' });
 });
 
 module.exports = router;
