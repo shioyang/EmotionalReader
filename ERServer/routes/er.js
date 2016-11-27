d547ad860c52fe4995f4dc398693905eb82e8eaf
Tomohiro SHIOYA <shioyang4321@gmail.com>
2016-11-27 21:33:08 +0900

    Implement sentence API

--- ERServer/routes/er.js
+++ ERServer/routes/er.js
@@ -2,19 +2,42 @@ var express = require('express');
 var router = express.Router();
 
 var indico = require('indico.io');
-//var response = function(res) { console.log(res); }
+indico.apiKey =  '';
+
 var logError = function(err) { console.log(err); }
 
-/* GET API list. */
+/* GET */
+/*
+ * Response
+ *   API list
+ */
 router.get('/', function(req, res, next) {
-  res.render('api_list', { title: 'ER API List' });
+  res.render('api_list', { title: 'ER API List' }); //TODO
+});
+
+/* GET */
+router.get('/emotion', function(req, res){
+  // single example
+  indico.emotion("I did it. I got into Grad School. Not just any program, but a GREAT program. :-)")
+    .then(function(res_emotion){
+      res.send(res_emotion);
+    })
+    .catch(logError);
 });
 
 /* POST */
-router.get('/emotion', function(req, res){
-//router.post('/emotion', function(req, res){
-  // single example
-  indico.emotion("I did it. I got into Grad School. Not just any program, but a GREAT program. :-)")
+/*
+ * Request params
+ *   sentence: string;
+ * Response
+ *   anger: number;
+ *   joy: number;
+ *   fear: number;
+ *   sadness: number;
+ *   surprise: number;
+ */
+router.post('/sentence', function(req, res){
+  indico.emotion(req.body.sentence)
     .then(function(res_emotion){
       res.send(res_emotion);
     })
