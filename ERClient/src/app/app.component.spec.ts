c6f51f1be530f2a5d15d60e21e3b5f1da2ea4bd4
Tomohiro SHIOYA <shioyang4321@gmail.com>
2016-12-20 22:41:04 +0900

    Update and fix UI specs

--- ERClient/src/app/app.component.spec.ts
+++ ERClient/src/app/app.component.spec.ts
@@ -1,14 +1,28 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async } from '@angular/core/testing';
+
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 import { AppComponent } from './app.component';
+import { ErService } from './er.service';
+import { EmotionToColorPipe } from './emotion-to-color.pipe';
 
 describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        EmotionToColorPipe
       ],
+      imports: [
+        FormsModule,
+        HttpModule
+      ],
+      providers: [
+        ErService
+      ]
     });
   });
 
@@ -18,16 +32,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have as title 'Emotional Reader'`, async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).toEqual('Emotional Reader');
   }));
 
   it('should render title in a h1 tag', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('h1').textContent).toContain('Emotional Reader');
   }));
 });
--- ERClient/src/app/er.service.spec.ts
+++ ERClient/src/app/er.service.spec.ts
@@ -1,11 +1,15 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
+
+import { HttpModule } from '@angular/http';
+
 import { ErService } from './er.service';
 
 describe('ErService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpModule],
       providers: [ErService]
     });
   });
