1bb6a15a7dadce48530d75dfa6827cc01906c8fc
Tomohiro SHIOYA <shioyang4321@gmail.com>
2017-01-02 17:41:52 +0900

    Use "split sentence" feature

--- src/routes/er.js
+++ src/routes/er.js
@@ -62,76 +62,23 @@ router.post('/sentence', function(req, res){
  *   sentence: string;
  */
 router.post('/paragraph', function(req, res){
-  let splitToSentences = function(paragraph){
-    // .
-    let sentenceArr = paragraph.split(".");
-    sentenceArr.forEach(function(sent, index){
-      if(sent != "" && index != sentenceArr.length - 1){
-        sentenceArr[index] = sent + ".";
-      }
-    });
-    // !
-    let newArr2 = [];
-    sentenceArr.forEach(function(sent){
-      let arr2 = sent.split("!");
-      arr2.forEach(function(sent2, index2){
-        if(sent2 != "" && index2 != arr2.length - 1){
-          arr2[index2] = sent2 + "!";
-        }
-      });
-      newArr2 = newArr2.concat(arr2);
-    });
-    sentenceArr = newArr2;
-    // ?
-    let newArr3 = [];
-    sentenceArr.forEach(function(sent){
-      let arr3 = sent.split("?");
-      arr3.forEach(function(sent3, index3){
-        if(sent3 != "" && index3 != arr3.length - 1){
-          arr3[index3] = sent3 + "?";
-        }
-      });
-      newArr3 = newArr3.concat(arr3);
-    });
-    sentenceArr = newArr3;
-    return sentenceArr;
-  }
-
   let paragraph = req.body.paragraph;
-  let sentenceArr = splitToSentences(paragraph);
-  let defArr = [];
   let res_emotions = [];
-  console.log(sentenceArr);
 
-  sentenceArr.forEach(function(sentence, index){
-    if(sentence === ""){ return; }
-    defArr.push(new Promise(function(resolve, reject){
-      indico.emotion(sentence)
-        .then(function(res_emotion){
-          res_emotion.index = index; // for sorting
-          res_emotion.sentence = sentence;
-          res_emotions.push(res_emotion);
-          resolve();
-        })
-        .catch(function(err){
-          console.log(err);
-          reject(err);
-        });
-    }));
-  })
-
-  Promise.all(defArr).then(function(){
-    res_emotions.sort(function(a, b){
-      return a.index - b.index;
+  indico.emotion(paragraph, { 'split': 'sentence' })
+    .then(function(emoArr){
+      console.log(emoArr);
+      emoArr.forEach(function(emo){
+        emo.results.sentence = emo.text;
+        res_emotions.push(emo.results);
+      });
+      res.send({
+        list: res_emotions
+      });
+    })
+    .catch(function(err){
+      console.warn(err);
     });
-    let obj = {
-      list: res_emotions
-    };
-    res.send(obj);
-  })
-  .catch(function(err){
-    console.log(err);
-  });
 });
 
 module.exports = router;
